0910d68e221495e1b21ddd13c5803b1c65928930
Bryan Garza <bgarza1016@gmail.com>
2015-08-25 09:06:23 -0700

    second pass still not getting angular YET

--- kitties-STARTER-app/js/KittiesController.js
+++ kitties-STARTER-app/js/KittiesController.js
@@ -5,13 +5,15 @@
 	.module("kittyApp")
 	.controller("KittiesController", KittiesController);
 
-	// KittiesController.$inject = [];
+	KittiesController.$inject = [];
 
 	function KittiesController() {
 		var vm = this;
 
+		vm.ids = 4;
 		vm.allKitties = [
 			{
+				id: 0,
 				catName: 'Karl',
 				monthsOld: 5,
 				gender: 'boy',
@@ -19,6 +21,7 @@
 				adopted: true,
 			},
 			{
+				id: 1,
 				catName: 'Jack',
 				monthsOld: 4,
 				gender: 'boy',
@@ -26,6 +29,7 @@
 				adopted: false
 			},
 			{
+				id:2,
 				catName: 'Oscar',
 				monthsOld: 2,
 				gender: 'boy',
@@ -33,6 +37,7 @@
 				adopted: false
 			},
 			{
+				id: 3,
 				catName: 'Princess Mew',
 				monthsOld: 3,
 				gender: 'girl',
@@ -40,7 +45,23 @@
 				adopted: false
 			}
 
-		];
+		]
+		vm.newKitty = function() {
+			this.allKitties.push({
+					id: this.ids,
+					catName: this.newKitty.catName,
+					monthsOld: this.newKitty.monthsOld,
+					gender: this.newKitty.gender,
+					image: this.newKitty.image,
+					adopted: this.newKitty.adopted
+				});
+				this.ids++;
+				this.newKitty.catName = '';
+				this.newKitty.monthsOld;
+				this.newKitty.gender = '';
+				this.newKitty.image = '';
+				this.newKitty.adopted = false;
+		}
 
 	};
 
